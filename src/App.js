0c3cda87986e07783428c0a576a59feae4810d78
Sarah Maris <sarah@sarahmaris.com>
2017-12-22 18:00:21 -0500

    Add default locations

--- src/App.js
+++ src/App.js
@@ -3,28 +3,45 @@ import logo from './logo.svg';
 import './App.css';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
+const locations = [
+          {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+          {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+          {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+          {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+          {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+          {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+        ];
+
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
   <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    ref={(ref) => { this.map = ref }}
+    defaultZoom={13}
+    defaultCenter={ {lat: 40.7413549, lng: -73.9980244}}
   >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+{locations.map(marker =>  {
+  return <Marker position={marker.location} key={marker.title} />})}
+
   </GoogleMap>
 ))
 
 
+
 class App extends Component {
   render() {
+
+
+
     return (
       <div className="App">
 
         <MyMapComponent
-          isMarkerShown
+
           googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD4Hf4D47wT3dI_iA6kyul2YFsvzjDMHFE"
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `400px` }} />}
           mapElement={<div style={{ height: `100%` }} />}
         />
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
