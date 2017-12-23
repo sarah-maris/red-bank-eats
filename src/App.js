bfb78188a47d52e875356e575cba15f7f2ee07aa
Sarah Maris <sarah@sarahmaris.com>
2017-12-23 13:06:57 -0500

    Add outside markers to test fitbounds

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const locations = [
           {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
           {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
           {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-          {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+          {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -74.9961237}}
         ];
 
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
