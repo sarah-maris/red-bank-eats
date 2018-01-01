dbd8d40214d9805f4415cb5ff8469e9cce4c8e4a
Sarah Maris <sarah@sarahmaris.com>
2017-12-31 21:22:10 -0500

    Update map styling

--- src/data/mapStyles.js
+++ src/data/mapStyles.js
@@ -1,160 +1,109 @@
-// Source: https://snazzymaps.com/style/133000/aguadebro1
-
-export const mapStyles = [
-    {
-        "featureType": "administrative.province",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "hue": "#a800ff"
-            },
-            {
-                "lightness": "75"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative.locality",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "hue": "#ffb900"
-            },
-            {
-                "visibility": "on"
-            },
-            {
-                "lightness": "3"
-            }
-        ]
-    },
-    {
-        "featureType": "administrative.land_parcel",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "hue": "#ccff00"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "all",
-        "stylers": [
-            {
-                "hue": "#FFBB00"
-            },
-            {
-                "saturation": 43.400000000000006
-            },
-            {
-                "lightness": 37.599999999999994
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "all",
-        "stylers": [
-            {
-                "hue": "#00FF6A"
-            },
-            {
-                "saturation": -1.0989010989011234
-            },
-            {
-                "lightness": 11.200000000000017
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "all",
-        "stylers": [
-            {
-                "hue": "#ffc200"
-            },
-            {
-                "saturation": -61.8
-            },
-            {
-                "lightness": 45.599999999999994
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "all",
-        "stylers": [
-            {
-                "hue": "#FF0300"
-            },
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 51.19999999999999
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "elementType": "all",
-        "stylers": [
-            {
-                "hue": "#FF0300"
-            },
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 52
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "all",
-        "stylers": [
-            {
-                "hue": "#0078ff"
-            },
-            {
-                "saturation": -13.200000000000003
-            },
-            {
-                "lightness": 2.4000000000000057
-            },
-            {
-                "gamma": 1
-            },
-            {
-                "weight": "4.61"
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    }
-]
+export const mapStyles = [{
+  "featureType": "water",
+  "elementType": "geometry.fill",
+  "stylers": [{
+    "color": "#3594c5"
+  }, {
+    "hue": "#3594c5"
+  }, {
+    "lightness": 3
+  }]
+}, {
+  "featureType": "landscape.natural.landcover",
+  "elementType": "geometry.fill",
+  "stylers": [{
+    "color": "#aee9c6"
+  }, {
+    "hue": "#aee9c6"
+  }]
+}, {
+  "featureType": "poi.park",
+  "elementType": "geometry.fill",
+  "stylers": [{
+    "color": "#aee9c6"
+  }]
+}, {
+  "featureType": "poi.sports_complex",
+  "elementType": "geometry.fill",
+  "stylers": [{
+    "color": "#aee9c6"
+  }, {
+    "hue": "#000000"
+  }]
+}, {
+  "featureType": "poi.park",
+  "elementType": "geometry",
+  "stylers": [{
+    "visibility": "simplified"
+  }, {
+    "color": "#aee9c6"
+  }, {
+    "lightness": 15
+  }]
+}, {
+  "featureType": "poi.medical",
+  "elementType": "all",
+  "stylers": [{
+    "visibility": "off"
+  }]
+}, {
+  "featureType": "poi.business",
+  "elementType": "all",
+  "stylers": [{
+    "visibility": "off"
+  }]
+}, {
+  "featureType": "poi.school",
+  "elementType": "all",
+  "stylers": [{
+    "visibility": "off"
+  }, {
+    "color": "#ffffff"
+  }]
+}, {
+  "featureType": "road.highway",
+  "elementType": "geometry",
+  "stylers": [{
+    "color": "#ffffff"
+  }]
+}, {
+  "featureType": "road.highway",
+  "elementType": "labels.text.stroke",
+  "stylers": [{
+    "visibility": "off"
+  }, {
+    "weight": 0.1
+  }]
+}, {
+  "featureType": "road.highway",
+  "elementType": "labels.text.fill",
+  "stylers": [{
+    "color": "#737373"
+  }]
+}, {
+  "featureType": "poi.park",
+  "elementType": "labels",
+  "stylers": [{
+    "visibility": "off"
+  }]
+}, {
+  "featureType": "landscape",
+  "elementType": "geometry.fill",
+  "stylers": [{
+    "visibility": "on"
+  }, {
+    "color": "#e0e0e0"
+  }]
+}, {
+  "featureType": "poi.place_of_worship",
+  "elementType": "all",
+  "stylers": [{
+    "visibility": "off"
+  }]
+}, {
+  "featureType": "poi",
+  "elementType": "all",
+  "stylers": [{
+    "visibility": "off"
+  }]
+}]
